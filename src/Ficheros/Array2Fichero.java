6377ecbc69ec5520c1e1f608b7bf2d42d1130419
zaineb88 <z.m.mala@hotmail.com>
2017-01-17 21:11:39 +0100

    ficheros

--- src/Ficheros/Array2Fichero.java
+++ src/Ficheros/Array2Fichero.java
@@ -1,64 +1,115 @@
 package Ficheros;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Array2Fichero {
-	
-	public static void main(String[] args) {
-		
-    String [] lista=fromFichero2Array("zaineb");
-		
-        for(int i=0;i<lista.length;i++)	
-        {
-        	System.out.println(lista[i]);
-        }
-        	
-	
-		
-		
-	}
-	
-	public static String [] fromFichero2Array(File file) throws FileNotFoundException
-	{  
-		String [] lista_cadena=null;
-		
-        FileReader file_reader = new FileReader(new File ("zaineb"));
-        int a = 0;
-        int suma = 0;
-        
-      
-        while (a != -1)
-        {
-            char c = (char) a; 
-            if(c != ' ' && a != 0)
-            {
-                System.out.println(c);
-             
-            }
-         //   a = file_reader.read();
-        }
-    
-     //   file_reader.close();
-    
 
-		
-		
-		return lista_cadena;
-		
+	public static void main(String[] args) throws IOException {
+
+		File file = null;
+		file = new File("zaineb");
+		int num = cuentaLineas(file);
+		String[] lista_cadena = new String[num];
+
+		lista_cadena = fromFichero2Array(file);
+
+		for (String cadena : lista_cadena) // for each es como for normal pero
+											// con otra manera
+		{
+			System.out.println(cadena);
+		}
+
+		/////////////////// lamada de 2 metodo
+
+		boolean res = fromArray2Fichero(lista_cadena);
+		System.out.println(res);
+		// fin de main
 	}
-	
-	
-	
-	public static boolean fromArray2Fichero(String [] lista_cadena)
-	{
-		boolean res=false;
+
+	public static String[] fromFichero2Array(File file) throws IOException {
+
+		int num_lineas = cuentaLineas(file);
+
+		String[] lista_cadena = null;
+		lista_cadena = new String[num_lineas];
+
+		String linea = "";
+
+		BufferedReader br = new BufferedReader(new FileReader(new File(file.getName()))); // FileReade para leer char BufferedReader para leer String
+
+		int i = 0;
+		linea = br.readLine();
+		while (linea != null) {
+			lista_cadena[i] = linea;
+			linea = br.readLine();
+			i++;
+		}
+
+		br.close();
+		return lista_cadena;
+
+	}
+
+	public static boolean fromArray2Fichero(String[] lista_cadena)  {
+		boolean res = true;
+		FileWriter fw = null; // para escribir en un fichero
+		BufferedWriter bw = null;
+
+		try {
+			fw = new FileWriter("ficheroCreado");
+			bw = new BufferedWriter(fw);
+
+			for (String cadena : lista_cadena) {
+				bw.write(cadena);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Fichero creado");
+			e.printStackTrace();
+		}
 		
+		finally {
+			
+		     
+		  try
 		
+		  {
+		
+			  bw.close();
+		
+			  fw.close();
+		
+		  }catch(Exception e)
+		
+		  {
+			  System.out.println("Error cerrando fichero salida");
+			  e.printStackTrace();
+			  res=false;
+		  }
+	}
 		return res;
 	}
 
-	
-	//fin de clase
+	//////////////////////////// metodo para contar las lineas de un fichero
+
+	public static int cuentaLineas(File file) throws IOException {
+		int num_lineas = 0;
+		FileReader file_reader = new FileReader(new File("zaineb"));
+		BufferedReader br = new BufferedReader(file_reader);
+
+		while (br.readLine() != null) {
+			num_lineas++;
+		}
+		br.close();
+
+		return num_lineas;
+	}
+
+	// fin de clase
 }
